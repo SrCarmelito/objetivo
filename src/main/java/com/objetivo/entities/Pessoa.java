8b8a460eb4107ce1654923731cf71e7bd471bdca
Carmelito Jr <carmelito.benali@hotmail.com>
2024-05-26 22:10:08 -0300

    Deploy Render Docker

--- src/main/java/com/objetivo/entities/Pessoa.java
+++ src/main/java/com/objetivo/entities/Pessoa.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import org.hibernate.annotations.Formula;
 import org.hibernate.validator.constraints.br.CPF;
 

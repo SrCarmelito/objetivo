04dde4fe8de19a9d87fd8754744dbd3bc2f77cf4
Carmelito Jr <carmelito.benali@hotmail.com>
2024-05-30 20:47:20 -0300

    profiles dev and prod

--- src/main/java/com/objetivo/profiles/ConfigDev.java
+++ src/main/java/com/objetivo/profiles/ConfigDev.java
@@ -1,16 +1,23 @@
 package com.objetivo.profiles;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @Profile("dev")
-public class ConfigDev {
+public class ConfigDev implements ApplicationRunner {
 
-    public ConfigDev() {
-        log.info("Profile de Dev");
+    @Value("${hello}")
+    String hello;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info(hello);
     }
 
 }

b7de1158b3c18aef8067dd858a7af7011250f401
Carmelito Jr <carmelito.benali@hotmail.com>
2024-05-31 12:04:54 -0300

    pontual corrections

--- cria_banco.sql
+++ cria_banco.sql
@@ -1629,3 +1629,7 @@ INSERT INTO elo.endereco (id, pessoa_id, cep, logradouro, numero, cidade, uf, ba
 INSERT INTO elo.endereco (id, pessoa_id, cep, logradouro, numero, cidade, uf, bairro) VALUES(998, 235, '87015000', 'RUA D. PEDRO II', '998', 'Crixás', 'SP', 'A DEFINIR!');
 INSERT INTO elo.endereco (id, pessoa_id, cep, logradouro, numero, cidade, uf, bairro) VALUES(999, 387, '87050000', 'Rua Afonso Pena', '999', 'SAO ROQUE DE MINAS', 'TO', 'A DEFINIR!');
 INSERT INTO elo.endereco (id, pessoa_id, cep, logradouro, numero, cidade, uf, bairro) VALUES(1000, 571, '13088061', 'AV DOS PALMARES', '1000', 'Juripiranga', 'AC', 'A DEFINIR!');
+
+select setval('s_pessoa', (select max(id) from elo.pessoa));
+
+select setval('s_endereco', (select max(id) from elo.endereco));

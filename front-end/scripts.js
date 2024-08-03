2b3a643ea972c602cceb6217e05eace7b0e395d4
Carmelito Jr <carmelito.benali@hotmail.com>
2024-08-03 10:23:49 -0300

    Melhorias Reports

--- front-end/scripts.js
+++ front-end/scripts.js
@@ -1,4 +1,4 @@
-const url = "https://objetivo-liv5.onrender.com/login";
+const url = "http://localhost:8080/login";
 
 document.querySelector("#submit").addEventListener("click", (e) => {
     e.preventDefault();
@@ -16,7 +16,7 @@ document.querySelector("#submit").addEventListener("click", (e) => {
 
 async function login(user) {
 
-    const response = await fetch("https://objetivo-liv5.onrender.com/login",
+    const response = await fetch("http://localhost:8080/login",
     {
         body: JSON.stringify(user),
         method: 'POST',
---
+++ src/main/java/com/objetivo/report/ReportService.java
@@ -0,0 +1,54 @@
+package com.objetivo.report;
+
+import com.objetivo.entities.Pessoa;
+import com.objetivo.service.PessoaService;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class ReportService {
+
+    public static final String JASPER_DIRETORIO = "reports/jasper/pessoa/";
+    public static final String JASPER_PREFIXO = "pessoa";
+    public static final String JASPER_SUFIXO = ".jasper";
+
+    private final PessoaService pessoaService;
+
+    public ReportService(PessoaService pessoaService) {
+        this.pessoaService = pessoaService;
+    }
+
+    private Map<String, Object> params = new HashMap<>();
+
+    public byte[] exportarPDF(Long id) {
+        Pessoa pessoa = this.pessoaService.obterPessoaPorId(id);
+
+        params.put("nome", pessoa.getNome());
+        params.put("cpf", pessoa.getCpf());
+        params.put("dataNascimento", pessoa.getDataNascimento());
+        params.put("telefone", pessoa.getTelefone());
+
+        byte[] bytes = null;
+        try {
+            File file = ResourceUtils.getFile(JASPER_DIRETORIO.concat(JASPER_PREFIXO.concat(JASPER_SUFIXO)));
+            JasperPrint print = JasperFillManager.fillReport(file.getAbsolutePath(), params, new JREmptyDataSource());
+            bytes = JasperExportManager.exportReportToPdf(print);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (JRException e) {
+            throw new RuntimeException(e);
+        }
+        return bytes;
+    }
+
+}
